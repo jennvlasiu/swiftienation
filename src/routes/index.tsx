499e2043f49ecba798675b5ead3bd4a5aa0167f3
jennvlasiu <jenvlasiu@gmail.com>
2024-04-28 09:30:52 -0400

    updated to make Taylor Swift specific

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <main>
       <Title>Swifties Nation</Title>
-      <h1>All things Taylor Swift, Easter Eggs and more! </h1>
+      <h1>Taylor x AI; your AI Swiftie Companion </h1>
       <Counter />
       <p>
-        Swiftie Nation, we've designed the ultimate AI on all things Tay tay. From playful references, detailed discussions about her lyrics,to music, and personal trivia. We maintain a tone reminiscent of Taylor Swift's style—witty, heartfelt, and occasionally reflective, perfect for engaging with you on a personal level and spreading positivity. We avoid negative comments about Taylor Swift and ensures all discussions are respectful and celebratory of her work. {""}
+          Swiftie Nation, we've designed the ultimate AI on all things Tay tay. From playful references, detailed discussions about her lyrics,to music, and personal trivia. We maintain a tone reminiscent of Taylor Swift's style—witty, heartfelt, and occasionally reflective, perfect for engaging with you on a personal level and spreading positivity. We avoid negative comments about Taylor Swift and ensures all discussions are respectful and celebratory of her work. {""}
         <a href="https://chat.openai.com/g/g-POSrA6oW8-tortured-poets-department" target="_blank">
-            Visit Tortured Poets Department to get started with your own Tay Tay Copilot
+            Visit Tortured Poets Department
         </a>{" "}
-        to interact with your ultimate Taylor AI companion.
+        to interact with your ultimate Taylor AI-enabled Swiftie
       </p>
     </main>
   );
