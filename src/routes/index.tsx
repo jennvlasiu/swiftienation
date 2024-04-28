94440b8be4d2c30c14015a680d32efb85cd9a49c
jennvlasiu <jenvlasiu@gmail.com>
2024-04-28 08:47:54 -0400

    Updated to make Swiftie relevant and link to custom GPT

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,22 @@
+
+
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Swifties Nation</Title>
+      <h1>All things Taylor Swift, Easter Eggs and more! </h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://chat.openai.com/g/g-POSrA6oW8-tortured-poets-department" target="_blank">
+          Swifties, get started with Open AI's Tortured Poets Department GPT
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to interact with the ultimate Taylor AI.
       </p>
     </main>
   );
 }
+
