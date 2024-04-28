dce21d654c74b46ed64d06306aa4d2445270ae4e
jennvlasiu <jenvlasiu@gmail.com>
2024-04-28 09:27:57 -0400

    updated to make Taylor Swift specific

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,9 +10,9 @@ export default function Home() {
       <h1>All things Taylor Swift, Easter Eggs and more! </h1>
       <Counter />
       <p>
-        Visit our OpenAI{" "}
+        Swiftie Nation, we've designed the ultimate AI on all things Tay tay. From playful references, detailed discussions about her lyrics,to music, and personal trivia. We maintain a tone reminiscent of Taylor Swift's style—witty, heartfelt, and occasionally reflective, perfect for engaging with you on a personal level and spreading positivity. We avoid negative comments about Taylor Swift and ensures all discussions are respectful and celebratory of her work. {""}
         <a href="https://chat.openai.com/g/g-POSrA6oW8-tortured-poets-department" target="_blank">
-            Taylor inspired Copilot
+            Visit Tortured Poets Department to get started with your own Tay Tay Copilot
         </a>{" "}
         to interact with your ultimate Taylor AI companion.
       </p>
