54feb9932cf8cfdaa99e1ec5816819cf0bdc4f7d
jennvlasiu <jenvlasiu@gmail.com>
2024-04-28 09:31:37 -0400

    updated to make Taylor Swift specific

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,7 +5,7 @@ export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
     <button class="increment" onClick={() => setCount(count() + 1)}>
-      I'm a Swiftie: {count()}
+      I'm a Swiftie, Click here: {count()}
     </button>
   );
 }
