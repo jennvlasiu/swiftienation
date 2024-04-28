7076e918837b9a6c3578011a1a96c3c6c2984919
jennvlasiu <jenvlasiu@gmail.com>
2024-04-28 09:42:17 -0400

    updated to make Taylor Swift specific

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,12 +6,13 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Swifties x AI </h1>
+      <h1>Page Not Found</h1>
       <p>
-        Generative Pre-trained Transformers, commonly known as GPT, are a family of neural network models that uses the transformer architecture and is a key advancement in artificial intelligence (AI) powering generative AI applications such as ChatGPT. GPT models give applications the ability to create human-like text and content (images, music, and more), and answer questions in a conversational manner. Organizations across industries are using GPT models and generative AI for Q&A bots, text summarization, content generation, and search.This custom GPT is designed to integrate Taylor Swift Easter eggs into conversations subtly and engagingly. It offers Swifties playful references, detailed discussions about her lyrics, music, and personal trivia. We've trained the GPT to maintain a tone reminiscent of Taylor Swift's style—witty, heartfelt, and occasionally reflective. We avoid negative comments about Taylor Swift and ensures all discussions are respectful and celebratory of her work. Visit {""}
-        <a href="https://chat.openai.com/g/g-POSrA6oW8-tortured-poets-department" target="_blank"
-           Tortured Poets Department </a>{" "}
-        to get started with your own Tay Tay Copilot
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
       </p>
     </main>
   );
