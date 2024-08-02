c9a6d6c58316e751a27d2a3fbc3c8f784237178d
jennvlasiu <jenvlasiu@gmail.com>
2024-08-02 14:22:34 -0400

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,18 +1,18 @@
 import { Title } from "solid-start";
 import { HttpStatusCode } from "solid-start/server";
 
-export default function NotFound() {
+export default function UnderConstruction() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>Page Under Construction</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Page Under Construction</h1>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+          taylorswift.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to view official updates
       </p>
     </main>
   );
